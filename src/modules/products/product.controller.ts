9f1cb57e55dd08b742ef2b71e30a9479b931aaf2
AkibKST <akibkst22@gmail.com>
2024-06-16 14:15:00 +0600

    create post, get all product, get by single id api

--- src/modules/products/product.controller.ts
+++ src/modules/products/product.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { ProductServices } from "./product.service";
 
+//product create
 const createProduct = async (req: Request, res: Response) => {
   const productData = req.body;
 
@@ -12,6 +13,47 @@ const createProduct = async (req: Request, res: Response) => {
   });
 };
 
+//get all product
+const getAllProducts = async (req: Request, res: Response) => {
+  try {
+    const result = await ProductServices.getAllProducts();
+
+    res.status(200).json({
+      success: true,
+      message: "Product are fetched successfully !",
+      data: result,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: "Could not fetch product!",
+      error: err,
+    });
+  }
+};
+
+//get product by single id
+const getProductById = async (req: Request, res: Response) => {
+  try {
+    const productId = req.params;
+    const result = await ProductServices.getProductById(productId);
+
+    res.status(200).json({
+      success: true,
+      message: "Movies are fetched successfully !",
+      data: result,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: "Could not fetch movies!",
+      error: err,
+    });
+  }
+};
+
 export const ProductControllers = {
   createProduct,
+  getAllProducts,
+  getProductById,
 };
--- src/modules/products/product.service.ts
+++ src/modules/products/product.service.ts
@@ -6,6 +6,18 @@ const createProduct = async (payLoad: TProduct) => {
   return result;
 };
 
+const getAllProducts = async () => {
+  const result = await Product.find();
+  return result;
+};
+
+const getProductById = async (productId: any) => {
+  const result = await Product.find(productId);
+  return result;
+};
+
 export const ProductServices = {
   createProduct,
+  getAllProducts,
+  getProductById,
 };
