ab6fed6bc4ccbe7b696f3036199a5332ba46fc69
AkibKST <akibkst22@gmail.com>
2024-06-16 13:54:28 +0600

    create interface+model+route+controller+service partter

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
 import express, { Request, Response } from "express";
+import { ProductRoutes } from "./modules/products/product.route";
 
 const app = express();
+
+//parsers
+app.use(express.json());
+
+//middleware
+app.use("/api/products", ProductRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello next World of my life");
